932aeaa486aad486ac5d4a41f7fa55011a9c0b38
mokohogastavias <mokohogastavias@gmail.com>
2025-10-26 20:39:43 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import WhatsAppFloat from './components/WhatsAppFloat'
 import Home from './pages/Home'
 import ProductDetail from './pages/ProductDetail'
 import Category from './pages/Category'
 import Cart from './pages/Cart'
+import About from './pages/About'
+import Terms from './pages/Terms'
+import Privacy from './pages/Privacy'
+import Returns from './pages/Returns'
 
 function App() {
   return (
@@ -17,9 +22,14 @@ function App() {
             <Route path="/product/:sku" element={<ProductDetail />} />
             <Route path="/category/:slug" element={<Category />} />
             <Route path="/cart" element={<Cart />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/terms" element={<Terms />} />
+            <Route path="/privacy" element={<Privacy />} />
+            <Route path="/returns" element={<Returns />} />
           </Routes>
         </main>
         <Footer />
+        <WhatsAppFloat />
       </div>
     </Router>
   )
