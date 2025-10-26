19a7b577c639ca9170a30bee0c4254f9a4b9a594
mokohogastavias <mokohogastavias@gmail.com>
2025-10-26 19:53:54 +0200

    Create Cart.jsx

---
+++ src/pages/Cart.jsx
@@ -0,0 +1,8 @@
+export default function Cart() {
+  return (
+    <div className="container" style={{ padding: 'var(--space-xl) 0' }}>
+      <h1>Shopping Cart</h1>
+      <p>Your cart is currently empty</p>
+    </div>
+  )
+}
